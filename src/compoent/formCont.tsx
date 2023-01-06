503012da9f26c1151a3283e1bda53db127153b64
asteve <asteve@localhost.localdomain>
2023-01-06 07:05:23 +0100

    add more test and type

--- src/compoent/formCont.tsx
+++ src/compoent/formCont.tsx
@@ -1,7 +1,16 @@
+import React, { useState } from "react"
 
 
-export const ModalBx = () => {
-    
+
+type IModalBx = {
+    setTitleValue:React.Dispatch<React.SetStateAction<string | undefined>>,
+    setStatus:React.Dispatch<React.SetStateAction<string | undefined>>
+}
+
+
+
+export const ModalBx = ({setTitleValue,setStatus}:IModalBx) => {
+   
 
 
     return (
@@ -12,14 +21,24 @@ export const ModalBx = () => {
             
             <form action="">
                 <label htmlFor="labelField">Title</label>
-                <input type="text" id="labelField" ></input>
+                <input onChange={(e) => {
+                    const currentElem = e.target as HTMLInputElement
+                    setTitleValue(currentElem.value)
+                }} type="text" id="labelField" ></input>
                
                 
                 <label htmlFor="status">status:</label>
-                <select name="" id="status" data-testid="status">
+                <select name="" id="status" data-testid="status" onChange={(e) => {
+                    
+                    const currentElement = e.target as HTMLSelectElement
+                    setStatus(currentElement?.value)
+                }}>
                     <option value="">incomplete</option>
                     <option value="">completed</option>
-                   </select>
+                </select>
+                
+                <button  data-testid="add-task">add Task</button>
+                <button>cancel</button>
             </form>
         </div>
     )
---
+++ src/compoent/taskCard.tsx
@@ -0,0 +1,12 @@
+
+
+
+export const TaskCard = () => {
+    
+
+    return (
+        <div  className="w-[100%] h-[100%] bg-[white]" data-testid="todo-container">
+            
+        </div>
+    )
+}
